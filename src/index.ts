e97fc06c521c57196646d6d1029f433c3d15b892
montmorill <montmorill@outlook.com>
2026-04-15 00:11:47 +0800

    Improve command response handling and content
    Updates the action handler to be asynchronous and properly check for session existence before sending messages. Enhances the Ark message content with more descriptive text and a subtitle, while ensuring all responses are sent correctly.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { h, Schema } from 'koishi'
 
 export const name = 'montmorill'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
@@ -14,18 +14,20 @@ export function apply(ctx: Context) {
 
   ctx.command('pbhh', '屏被轰坏！')
     .alias('屏被轰坏', '㵗㶔𤃫𣸎')
-    .action(({ session }) => {
-      session?.send(`${h('img', { src: 'https://pbhh.net/㵗㶔𤃫𣸎.png' })}你的屏幕已被我轰坏！`)
-      return h('qq:ark24', {
+    .action(async ({ session }) => {
+      if (!session)
+        return
+      await session.send(`${h('img', { src: 'https://pbhh.net/㵗㶔𤃫𣸎.png' })}你的屏幕已被我轰坏！`)
+      await session.send(h('qq:ark24', {
         desc: '㵗㶔𤃫𣸎，溃濩泧漷。潏湟淴泱，㶖㴸㶒瀹。漩澴荥瀯，渨㵽濆瀑。',
         prompt: '我是海狶，你的屏幕已被我轰坏！',
         title: '平渹网',
-        metaDesc: '你的屏幕已被我轰坏！',
+        metaDesc: '㵗㶔𤃫𣸎，溃濩泧漷。潏湟淴泱，㶖㴸㶒瀹。漩澴荥瀯，渨㵽濆瀑。',
         img: 'https://pbhh.net/icons/icon-512.png',
         // link: 'mqqapi://openhalfscreenweb/?height=1920&url=https%3A%2F%2Fpbhh.net',
         link: 'https://pbhh.net',
-        // subTitle: 'adapter-qq-crack',
-      })
+        subTitle: 'pbhh.net',
+      }))
     })
 }
 
